9c44d8d104d98e179d51f3dac4a9e037a3c4a62e
LemonDuck <67409809+BetterDarkDuck@users.noreply.github.com>
2023-11-28 15:44:07 +0800

    更新 Grow.js

--- Rewrite/Grow.js
+++ Rewrite/Grow.js
@@ -11,9 +11,49 @@
 
 [rewrite_local]
 # > Grow解锁永久会员
-^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/GrowForeverVipCrack.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/BetterDarkDuck/Rules-For-Quantumult-X/master/Rewrite/Grow.js
 
 [mitm] 
 hostname = api.revenuecat.com
 
 *******************************/
+
+var note = {
+  "request_date": "2023-11-28T07:09:38Z",
+  "request_date_ms": 1701155378974,
+  "subscriber": {
+    "entitlements": {
+      "grow.pro": {
+        "expires_date": "2024-11-27T06:58:42Z",
+        "grace_period_expires_date": null,
+        "product_identifier": "grow_1y_128",
+        "purchase_date": "2023-11-28T06:58:42Z"
+      }
+    },
+    "first_seen": "2023-11-28T06:50:55Z",
+    "last_seen": "2023-11-28T06:50:55Z",
+    "management_url": "https://apps.apple.com/account/subscriptions",
+    "non_subscriptions": {},
+    "original_app_user_id": "$RCAnonymousID:3c15b483f9ca4f46bd483cf946901185",
+    "original_application_version": "309",
+    "original_purchase_date": "2023-11-19T09:32:55Z",
+    "other_purchases": {},
+    "subscriptions": {
+      "grow_1y_128": {
+        "auto_resume_date": null,
+        "billing_issues_detected_at": null,
+        "expires_date": "2024-11-27T06:58:42Z",
+        "grace_period_expires_date": null,
+        "is_sandbox": false,
+        "original_purchase_date": "2023-11-28T06:58:43Z",
+        "ownership_type": "PURCHASED",
+        "period_type": "trial",
+        "purchase_date": "2023-11-28T06:58:42Z",
+        "refunded_at": null,
+        "store": "app_store",
+        "store_transaction_id": "320001640889685",
+        "unsubscribe_detected_at": null
+      }
+    }
+  }
+}
